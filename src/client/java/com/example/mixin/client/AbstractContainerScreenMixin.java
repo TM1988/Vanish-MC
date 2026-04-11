01a80ee6a402d62cfc672b64e7a8a1b59cc00564
Tanish Mantri <tmgd.og1@gmail.com>
2026-04-10 22:58:47 -0500

    fixed trash slot single-click snapback by consuming paired release event

--- src/client/java/com/example/mixin/client/AbstractContainerScreenMixin.java
+++ src/client/java/com/example/mixin/client/AbstractContainerScreenMixin.java
@@ -17,6 +17,7 @@ public class AbstractContainerScreenMixin {
 	private static final int TRASH_SLOT_X = 178;
 	private static final int TRASH_SLOT_Y = 130;
 	private static final int TRASH_SLOT_INDEX = 46;
+	private boolean vanish$consumeNextRelease;
 
 	@Inject(method = "hasClickedOutside", at = @At("HEAD"), cancellable = true)
 	private void vanish$includeTrashSlotAsInside(double mouseX, double mouseY, int leftPos, int topPos, CallbackInfoReturnable<Boolean> cir) {
@@ -62,6 +63,17 @@ public class AbstractContainerScreenMixin {
 
 		int button = event.button() == 1 ? 1 : 0;
 		client.gameMode.handleContainerInput(screen.getMenu().containerId, TRASH_SLOT_INDEX, button, ContainerInput.PICKUP, client.player);
+		vanish$consumeNextRelease = true;
+		cir.setReturnValue(true);
+	}
+
+	@Inject(method = "mouseReleased", at = @At("HEAD"), cancellable = true)
+	private void vanish$consumeTrashSlotRelease(MouseButtonEvent event, CallbackInfoReturnable<Boolean> cir) {
+		if (!vanish$consumeNextRelease) {
+			return;
+		}
+
+		vanish$consumeNextRelease = false;
 		cir.setReturnValue(true);
 	}
 }
