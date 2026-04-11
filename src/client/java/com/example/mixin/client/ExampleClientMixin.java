81c8baec5bf6b30f91887a7dbde2fe6555a03f4f
Tanish Mantri <tmgd.og1@gmail.com>
2026-04-10 20:01:37 -0500

    fixed trash icon render to use B.png at strict 18x18 with no fullscreen panel

--- src/client/java/com/example/mixin/client/ExampleClientMixin.java
+++ src/client/java/com/example/mixin/client/ExampleClientMixin.java
@@ -18,7 +18,8 @@ public class ExampleClientMixin {
 	private static final int TRASH_SLOT_INDEX = 46;
 	private static final int INVENTORY_WIDTH = 176;
 	private static final int INVENTORY_HEIGHT = 166;
-	private static final Identifier TRASH_SLOT_TEXTURE = Identifier.fromNamespaceAndPath("vanish", "textures/gui/trash_slot.png");
+	private static final Identifier TRASH_SLOT_TEXTURE = Identifier.fromNamespaceAndPath("vanish", "textures/gui/B.png");
+	private static final int TRASH_TEXTURE_SIZE = 2000;
 
 	@Inject(method = "extractBackground", at = @At("TAIL"))
 	private void vanish$drawInventorySidePanel(GuiGraphicsExtractor extractor, int mouseX, int mouseY, float tickDelta, CallbackInfo ci) {
@@ -31,8 +32,7 @@ public class ExampleClientMixin {
 		int slotX = leftPos + TRASH_SLOT_X;
 		int slotY = topPos + TRASH_SLOT_Y;
 
-		drawConnectedPanel(extractor, leftPos, slotX, slotY);
-		extractor.blit(TRASH_SLOT_TEXTURE, slotX, slotY, TRASH_SLOT_SIZE, TRASH_SLOT_SIZE, 0.0F, 0.0F, 18.0F, 18.0F);
+		extractor.blit(TRASH_SLOT_TEXTURE, slotX, slotY, 0, 0, TRASH_SLOT_SIZE, TRASH_SLOT_SIZE, TRASH_TEXTURE_SIZE, TRASH_TEXTURE_SIZE);
 
 		InventoryScreen screen = (InventoryScreen) (Object) this;
 		if (!(screen.getMenu() instanceof InventoryMenu)) {
@@ -40,13 +40,4 @@ public class ExampleClientMixin {
 		}
 	}
 
-	private static void drawConnectedPanel(GuiGraphicsExtractor extractor, int leftPos, int slotX, int slotY) {
-		int plateX1 = leftPos + INVENTORY_WIDTH - 1;
-		int plateY1 = slotY - 2;
-		int plateX2 = slotX + TRASH_SLOT_SIZE;
-		int plateY2 = slotY + TRASH_SLOT_SIZE + 2;
-
-		extractor.fill(plateX1, plateY1, plateX2, plateY2, 0xFFC6C6C6);
-		extractor.outline(plateX1, plateY1, plateX2 - plateX1, plateY2 - plateY1, 0xFFE0E0E0);
-	}
 }
